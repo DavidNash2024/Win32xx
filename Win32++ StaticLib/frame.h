cc09290e2c2f0d7248d171bcb2a3ca38346141be
david_nash <david_nash@40094c20-7226-0410-9e7a-ca24b3fb783e>
2009-10-15 02:32:28 +0000

    Renaming of several samples. Adding new directories

---
+++ Win32++ StaticLib/frame.h
@@ -0,0 +1,327 @@
+// Win32++  Version 6.61
+// Released: 30th August, 2009 by:
+//
+//      David Nash
+//      email: dnash@bigpond.net.au
+//      url: https://sourceforge.net/projects/win32-framework
+//
+//
+// Copyright (c) 2005-2009  David Nash
+//
+// Permission is hereby granted, free of charge, to
+// any person obtaining a copy of this software and
+// associated documentation files (the "Software"),
+// to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////
+// frame.h
+//  Declaration of the CFrame and CMenubar classes
+
+// The classes declared in this file support SDI (Single Document Interface)
+// frames on Win32/Win64 operating systems (not Windows CE). For Windows CE,
+// use wceframe.h instead. SDI frames are a simple frame which supports a
+// single view window. Refer to mdi.h for frames that support several
+// child windows.
+
+// CFrame also includes each of the following classes as members:
+// * CRebar for managing the frame's rebar control.
+// * CMenubar for managing the menu inside the rebar.
+// * CToolbar for managing the frame's toolbar.
+// * CStatusbar for managing the frame's status bar.
+// In each case these members are exposed by a GetXXX function, allowing
+// them to be accessed or sent messages.
+
+// CFrame is responsible for creating a "frame" window. This window has a
+// menu and and several child windows, including a toolbar (usualy hosted 
+// within a rebar), and a status bar and a view positioned over the frame
+// window's non-client area. The "view" window is a seperate CWnd object 
+// assigned to the frame with the SetView function.
+
+// When compiling an application with these classes, it will need to be linked
+// with Comctl32.lib.
+
+// To create a SDI frame application, inherit a CMainFrame class from CFrame.
+// Use the Frame generic application as the starting point for your own frame
+// applications.
+// Refer to the Notepad and Scribble samples for examples on how to use these
+// classes to create a frame application.
+
+
+#ifndef _FRAME_H_
+#define _FRAME_H_
+
+#include "wincore.h"
+#include "dialog.h"
+#include "gdi.h"
+#include "statusbar.h"
+#include "toolbar.h"
+#include "rebar.h"
+#include <shlwapi.h>
+#include "Default_Resource.h"
+
+#ifndef RBN_MINMAX
+  #define RBN_MINMAX (RBN_FIRST - 21)
+#endif
+
+
+
+namespace Win32xx
+{
+
+	////////////////////////////////////////////////
+	// Declarations for structures for themes
+	//
+	struct ThemeMenu
+	{
+		BOOL UseThemes;			// TRUE if themes are used
+		COLORREF clrHot1;		// Colour 1 for top menu. Color of selected menu item
+		COLORREF clrHot2;		// Colour 2 for top menu. Color of checkbox
+		COLORREF clrPressed1;	// Colour 1 for pressed top menu and side bar
+		COLORREF clrPressed2;	// Colour 2 for pressed top menu and side bar
+		COLORREF clrOutline;	// Colour for border outline
+	};
+
+
+	// Forward declaration of CFrame. Its defined later.
+	class CFrame;
+
+
+	////////////////////////////////////
+	// Declaration of the CMenubar class
+	//
+	class CMenubar : public CToolbar
+	{
+	public:
+		CMenubar();
+		virtual ~CMenubar();
+		virtual BOOL IsMenubar() const {return TRUE;}
+		void MenuChar(WPARAM wParam, LPARAM lParam);
+		void SysCommand(WPARAM wParam, LPARAM lParam);
+		HMENU GetMenu() const {return m_hTopMenu;}
+		void SetMenu(HMENU hMenu);
+		ThemeMenu& GetMenubarTheme() {return m_ThemeMenu;}
+		void SetMenubarTheme(ThemeMenu& Theme);
+
+	protected:
+	//Overridables
+		virtual void OnCreate();
+		virtual LRESULT OnCustomDraw(NMHDR* pNMHDR);
+		virtual void OnKeyDown(WPARAM wParam, LPARAM lParam);
+		virtual void OnLButtonDown(WPARAM wParam, LPARAM lParam);
+		virtual void OnLButtonUp(WPARAM wParam, LPARAM lParam);
+		virtual void OnMouseLeave();
+		virtual void OnMouseMove(WPARAM wParam, LPARAM lParam);
+		virtual BOOL OnMenuInput(UINT uMsg, WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnNotifyReflect(WPARAM wParam, LPARAM lParam);
+		virtual void OnWindowPosChanged();
+		virtual void PreCreate(CREATESTRUCT &cs);
+		virtual LRESULT WndProcDefault(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	private:
+		CMenubar(const CMenubar&);				// Disable copy construction
+		CMenubar& operator = (const CMenubar&); // Disable assignment operator
+		void DoAltKey(WORD KeyCode);
+		void DoPopupMenu();
+		void DrawAllMDIButtons(CDC& DrawDC);
+		void DrawMDIButton(CDC& DrawDC, int iButton, UINT uState);
+		void ExitMenu();
+		HWND GetActiveMDIChild();
+		void GrabFocus();
+		BOOL IsMDIChildMaxed() const;
+		BOOL IsMDIFrame() const;
+		void ReleaseFocus();
+		void SetHotItem(int nHot);
+		static LRESULT CALLBACK StaticMsgHook(int nCode, WPARAM wParam, LPARAM lParam);
+
+		enum MDIButtonType
+		{
+			MDI_MIN = 0,
+			MDI_RESTORE = 1,
+			MDI_CLOSE = 2,
+		};
+
+		BOOL  m_bExitAfter;		// Exit after Popup menu ends
+		BOOL  m_bKeyMode;		// keyboard navigation mode
+		BOOL  m_bMenuActive;	// popup menu active
+		BOOL  m_bSelPopup;		// a popup (cascade) menu is selected
+		HMENU m_hPopupMenu;		// handle to the popup menu
+		HMENU m_hSelMenu;		// handle to the casceded popup menu
+		HMENU m_hTopMenu;		// handle to the top level menu
+		HWND  m_hPrevFocus;		// handle to window which had focus
+		CRect m_MDIRect[3];		// array of CRect for MDI buttons
+		int   m_nHotItem;		// hot item
+		int   m_nMDIButton;		// the MDI button (MDIButtonType) pressed
+		CPoint m_OldMousePos;	// old Mouse position
+		ThemeMenu m_ThemeMenu;	// Theme structure
+		CFrame* m_pFrame;       // Pointer to the frame.
+
+
+	};  // class CMenubar
+
+	//////////////////////////////////
+	// Declaration of the CFrame class
+	//
+	class CFrame : public CWnd
+	{
+		friend class CMenubar;
+
+	public:
+		CFrame();
+		virtual ~CFrame();
+
+		// Override these functions as required
+		virtual void AdjustFrameRect(RECT rcView) const;
+		virtual CRect GetViewRect() const;
+		virtual void SetStatusIndicators();
+		virtual void SetStatusText();
+		virtual void RecalcLayout();
+
+		// Virtual Attributes
+		// If you need to modify the default behaviour of the menubar, rebar, 
+		// statusbar or toolbar, inherrit from those classes, and override 
+		// the following attribute functions.
+		virtual CMenubar& GetMenubar() const		{return (CMenubar&)m_Menubar;}
+		virtual CRebar& GetRebar() const			{return (CRebar&)m_Rebar;}
+		virtual CStatusbar& GetStatusbar() const	{return (CStatusbar&)m_Statusbar;}
+		virtual CToolbar& GetToolbar() const		{return (CToolbar&)m_Toolbar;}
+
+		// These functions aren't virtual, and shouldn't be overridden
+		HMENU GetFrameMenu() const	{return m_hMenu;}
+		const ThemeMenu& GetMenuTheme()	const {return m_ThemeMenu;}
+		tString GetRegistryKeyName(){return m_tsKeyName;}
+		CWnd* GetView() const		{return m_pView;}
+		tString GetMRUEntry(size_t nIndex);
+		void SetFrameMenu(INT ID_MENU);
+		void SetMenuTheme(ThemeMenu& Theme);
+		void SetView(CWnd& wndView);
+		BOOL IsFrame() const			{return TRUE;}
+		BOOL IsMenubarUsed() const		{return (GetMenubar() != 0);}
+		BOOL IsRebarSupported() const	{return (GetComCtlVersion() >= 470);}
+		BOOL IsRebarUsed() const		{return (GetRebar() != 0);}
+
+	protected:
+		// Override these functions as required
+		virtual BOOL AddMenuIcon(int nID_MenuItem, HICON hIcon, int cx = 16, int cy = 16);
+		virtual size_t AddMenuIcons(const std::vector<UINT>& MenuData, COLORREF crMask, UINT ToolbarID, UINT ToolbarDisabledID);
+		virtual void AddMenubarBand();
+		virtual void AddMRUEntry(LPCTSTR szMRUEntry);
+		virtual void AddToolbarBand(CToolbar& TB, DWORD dwStyle, UINT nID);
+		virtual void AddToolbarButton(UINT nID, BOOL bEnabled = TRUE, LPCTSTR szText = 0);
+		virtual void CreateToolbar();
+		virtual void DrawCheckmark(LPDRAWITEMSTRUCT pdis);
+		virtual void DrawMenuIcon(LPDRAWITEMSTRUCT pdis, BOOL bDisabled);
+		virtual void DrawMenuText(CDC& DrawDC, LPCTSTR ItemText, CRect& rc, COLORREF colorText);
+		virtual int  GetMenuItemPos(HMENU hMenu, LPCTSTR szItem);
+		virtual void LoadRegistrySettings(LPCTSTR szKeyName);
+		virtual void LoadRegistryMRUSettings(UINT nMaxMRU = 0);
+		virtual void OnActivate(WPARAM wParam, LPARAM lParam);
+		virtual void OnClose();
+		virtual void OnCreate();
+		virtual LRESULT OnDrawItem(WPARAM wParam, LPARAM lParam);
+		virtual void OnExitMenuLoop();
+		virtual void OnHelp();
+		virtual void OnInitMenuPopup(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnMeasureItem(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnMenuChar(WPARAM wParam, LPARAM lParam);
+		virtual void OnMenuSelect(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnNotify(WPARAM wParam, LPARAM lParam);
+		virtual void OnSetFocus();
+		virtual void OnSysColorChange();
+		virtual LRESULT OnSysCommand(WPARAM wParam, LPARAM lParam);
+		virtual	void OnTimer(WPARAM wParam);
+		virtual void OnViewStatusbar();
+		virtual void OnViewToolbar();
+		virtual void PreCreate(CREATESTRUCT& cs);
+		virtual void PreRegisterClass(WNDCLASS &wc);
+		virtual BOOL PreTranslateMessage(MSG* pMsg);
+		virtual void RemoveMRUEntry(LPCTSTR szMRUEntry);
+		virtual void SaveRegistrySettings();
+		virtual void SetMenubarBandSize();
+		virtual size_t SetMenuIcons(const std::vector<UINT>& MenuData, COLORREF crMask, UINT ToolbarID, UINT ToolbarDisabledID);
+		virtual void SetupToolbar();
+		virtual void SetTheme();	
+		virtual void SetToolbarImages(COLORREF crMask, UINT ToolbarID, UINT ToolbarHotID, UINT ToolbarDisabledID);
+		virtual void ShowStatusbar(BOOL bShow);
+		virtual void ShowToolbar(BOOL bShow);
+		virtual void UpdateMRUMenu();
+		virtual LRESULT WndProcDefault(UINT uMsg, WPARAM wParam, LPARAM lParam);	
+
+		enum Constants
+		{
+			ID_STATUS_TIMER = 1,
+			POST_TEXT_GAP   = 16,			// for owner draw menu item
+		};
+
+		struct ItemData
+		// Each Dropdown menu item has this data
+		{
+			HMENU hMenu;
+			UINT  nPos;
+			UINT  fType;
+			TCHAR Text[MAX_MENU_STRING];
+			HMENU hSubMenu;
+		};
+
+        BOOL m_bShowIndicatorStatus;		// set to TRUE to see indicators in status bar
+		BOOL m_bShowMenuStatus;				// set to TRUE to see menu and toolbar updates in status bar
+		BOOL m_bUseRebar;					// set to TRUE if Rebars are to be used
+		BOOL m_bUseThemes;					// set to TRUE if themes are to be used
+		BOOL m_bUpdateTheme;				// set to TRUE to run SetThemes when theme changes
+		BOOL m_bUseToolbar;					// set to TRUE if the toolbar is used
+		BOOL m_bShowStatusbar;
+		BOOL m_bShowToolbar;
+		ThemeMenu m_ThemeMenu;				// Theme structure
+		HIMAGELIST m_himlMenu;				// Imagelist of menu icons
+		HIMAGELIST m_himlMenuDis;			// Imagelist of disabled menu icons
+
+	private:
+		CFrame(const CFrame&);				// Disable copy construction
+		CFrame& operator = (const CFrame&); // Disable assignment operator
+		void LoadCommonControls();
+
+		std::vector<ItemData*> m_vMenuItemData;// vector of ItemData pointers
+		std::vector<UINT> m_vMenuIcons;		// vector of menu icon resource IDs
+		CDialog* m_pAboutDialog;			// Pointer to the about dialog object
+		CMenubar m_Menubar;					// CMenubar object
+		CRebar m_Rebar;						// CRebar object
+		CStatusbar m_Statusbar;				// CStatusbar object
+		CToolbar m_Toolbar;					// CToolbar object
+		HMENU m_hMenu;						// handle to the frame menu
+		CWnd* m_pView;						// pointer to the View CWnd object
+		LPCTSTR m_OldStatus[3];				// Array of TCHAR pointers;
+		std::vector<tString> m_vMRUEntries;	// Vector of tStrings for MRU entires
+		tString m_tsKeyName;				// TCHAR std::string for Registry key name
+		tString m_tsStatusText;				// TCHAR std::string for status text
+		size_t m_nMaxMRU;					// maximum number of MRU entries
+		CRect m_rcPosition;					// CRect of the starting window position
+		HWND m_hOldFocus;					// The window which had focus prior to the app'a deactivation
+		int m_nOldID;
+
+	};  // class CFrame
+
+}
+
+
+#endif // _FRAME_H_
---
+++ Win32++ StaticLib/tab.h
@@ -0,0 +1,208 @@
+// Win32++  Version 6.61
+// Released: 30th August, 2009 by:
+//
+//      David Nash
+//      email: dnash@bigpond.net.au
+//      url: https://sourceforge.net/projects/win32-framework
+//
+//
+// Copyright (c) 2005-2009  David Nash
+//
+// Permission is hereby granted, free of charge, to
+// any person obtaining a copy of this software and
+// associated documentation files (the "Software"),
+// to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////
+// tab.h
+//  Declaration of the CTab and CMDITab classes
+
+#ifndef _TAB_H_
+#define _TAB_H_
+
+#include "wincore.h"
+#include "dialog.h"
+#include "gdi.h"
+#include "Default_Resource.h"
+
+namespace Win32xx
+{
+
+	struct TabPageInfo
+	{
+		TCHAR szTitle[MAX_MENU_STRING];
+		int iImage;
+		int nID;
+		CWnd* pWnd;
+	};
+
+	class CTab : public CWnd
+	{
+	protected:
+		// Declaration of the CSelectDialog class, a nested class of CTab
+		// It creates the dialog to choose which tab to activate
+		class CSelectDialog : public CDialog
+		{
+		public:
+			CSelectDialog(LPCDLGTEMPLATE lpTemplate, HWND hWndParent = NULL);
+			virtual ~CSelectDialog() {}
+			virtual void AddItem(LPCTSTR szString);
+			virtual BOOL IsTab() const { return FALSE; }
+
+		protected:
+			virtual BOOL OnInitDialog();
+			virtual void OnOK();
+			virtual void OnCancel() { EndDialog(-2); }
+
+		private:
+			CSelectDialog(const CSelectDialog&);				// Disable copy construction
+			CSelectDialog& operator = (const CSelectDialog&); // Disable assignment operator
+
+			std::vector<tString> m_vItems;
+			int IDC_LIST;
+
+		};
+	public:
+		CTab();
+		virtual ~CTab();
+		virtual int  AddTabPage(TabPageInfo& tbi);
+		virtual int  AddTabPage(CWnd* pWnd, LPCTSTR szTitle, HICON hIcon);
+		virtual int  AddTabPage(CWnd* pWnd, LPCTSTR szTitle, UINT nID_Icon);
+		virtual CRect GetCloseRect();
+		virtual CRect GetListRect();
+		virtual BOOL GetTabsAtTop();
+		virtual int  GetTabIndex(CWnd* pWnd);
+		virtual TabPageInfo GetTabPageInfo(UINT nTab);
+		virtual void RecalcLayout();
+		virtual void RemoveTabPage(int iPage);
+		virtual void SelectPage(int iPage);
+		virtual void SetShowButtons(BOOL bShow);
+		virtual void SetTabImage(UINT nTab, int iImage);
+		virtual void SetTabsAtTop(BOOL bTop);
+		virtual void SetTabText(UINT nTab, LPCTSTR szText);
+		virtual void SwapTabs(UINT nTab1, UINT nTab2);
+
+		// Attributes
+		std::vector <TabPageInfo>& GetAllTabs() const { return (std::vector <TabPageInfo>&) m_vTabPageInfo; }
+		HIMAGELIST GetImageList() const { return m_himlTab; }
+		BOOL GetShowButtons() const { return m_bShowButtons; }
+		int GetTabHeight() const { return m_nTabHeight; }
+		CWnd* GetActiveView() const		{ return m_pView; }
+		void SetTabHeight(int nTabHeight) { m_nTabHeight = nTabHeight; NotifyChanged();}
+
+		// Wrappers for Win32 Macros
+		void AdjustRect(BOOL fLarger, RECT *prc);
+		int  GetCurFocus();
+		int  GetCurSel();
+		BOOL GetItem(int iItem, LPTCITEM pitem);
+		int  GetItemCount();
+		int  HitTest(TCHITTESTINFO& info);
+		void SetCurFocus(int iItem);
+		int  SetCurSel(int iItem);
+		DWORD SetItemSize(int cx, int cy);
+		int  SetMinTabWidth(int cx);
+		void SetPadding(int cx, int cy);
+
+	protected:
+		virtual SIZE    GetMaxTabSize();
+		virtual void    OnCreate();
+		virtual void    OnLButtonDown(WPARAM wParam, LPARAM lParam);
+		virtual void    OnLButtonUp(WPARAM wParam, LPARAM lParam);
+		virtual void    OnMouseLeave(WPARAM wParam, LPARAM lParam);
+		virtual void    OnMouseMove(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnNCHitTest(WPARAM wParam, LPARAM lParam);
+		virtual void    PreCreate(CREATESTRUCT& cs);
+		virtual void    SetTabSize();
+		virtual LRESULT WndProcDefault(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	private:
+		CTab(const CTab&);				// Disable copy construction
+		CTab& operator = (const CTab&); // Disable assignment operator
+
+		void DrawCloseButton(CDC& DrawDC);
+		void DrawListButton(CDC& DrawDC);
+		void DrawTabs(CDC& dcMem);
+		void DrawTabBorders(CDC& dcMem, CRect& rcTab);
+		void Paint();
+		void NotifyChanged();
+		void SetActiveView(CWnd& Wnd);
+		void ShowListDialog();
+		void ShowListMenu();
+
+		std::vector<TabPageInfo> m_vTabPageInfo;
+		HIMAGELIST m_himlTab;
+		CWnd* m_pView;
+		BOOL m_bShowButtons;	// Show or hide the close and list button
+		BOOL m_IsTracking;
+		BOOL m_IsClosePressed;
+		BOOL m_IsListPressed;
+		BOOL m_IsListMenuActive;
+		int m_nTabHeight;
+	};
+
+	////////////////////////////////////////
+	// Declaration of the CTabbedMDI class
+	class CTabbedMDI : public CWnd
+	{
+	public:
+		CTabbedMDI();
+		virtual ~CTabbedMDI();
+		virtual CWnd* AddMDIChild(CWnd* pWnd, LPCTSTR szTabText, int nID = 0);
+		virtual void  CloseActiveMDI();
+		virtual void  CloseAllMDIChildren();
+		virtual void  CloseMDIChild(int nTab);
+		virtual CWnd* GetActiveMDIChild();
+		virtual CWnd* GetMDIChild(int nTab) {return GetTab().GetTabPageInfo(nTab).pWnd;}
+		virtual int   GetMDIChildCount();
+		virtual int   GetMDIChildID(int nTab) {return GetTab().GetTabPageInfo(nTab).nID;}
+		virtual LPCTSTR GetMDIChildTitle(int nTab) {return GetTab().GetTabPageInfo(nTab).szTitle;}
+		virtual CTab& GetTab() const	{return (CTab&)m_Tab;}
+		virtual BOOL IsTabbedMDI() const {return TRUE;}
+		virtual BOOL LoadRegistrySettings(tString tsRegistryKeyName);
+		virtual void RecalcLayout();
+		virtual void SaveRegistrySettings(tString tsRegistryKeyName);
+		virtual void SetActiveMDIChild(CWnd* pWnd);
+		virtual void SetActiveMDITab(int nTab);
+
+	protected:
+		virtual HWND    Create(HWND hWndParent);
+		virtual CWnd*   NewMDIChildFromID(int nID);
+		virtual void    OnDestroy(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnEraseBkGnd(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT OnNotify(WPARAM wParam, LPARAM lParam);
+		virtual void    OnWindowPosChanged(WPARAM wParam, LPARAM lParam);
+		virtual LRESULT WndProcDefault(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	private:
+		CTabbedMDI(const CTabbedMDI&);				// Disable copy construction
+		CTabbedMDI& operator = (const CTabbedMDI&); // Disable assignment operator
+
+		CTab m_Tab;
+	};
+	
+}
+
+
+#endif  // _TAB_H_
