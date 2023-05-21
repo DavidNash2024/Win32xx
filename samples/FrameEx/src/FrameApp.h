34268e1103ae17f46b18a4047cf448fcf96917fd
david_nash <david_nash@40094c20-7226-0410-9e7a-ca24b3fb783e>
2023-05-21 14:50:24 +0000

    Added the FrameEx sample which uses PNG images for the toolbar and menu item icons.

---
+++ samples/FrameEx/src/FrameApp.h
@@ -0,0 +1,33 @@
+/////////////////////////////
+// FrameApp.h
+//
+
+#ifndef FRAMEAPP_H
+#define FRAMEAPP_H
+
+#include "Mainfrm.h"
+
+
+////////////////////////////////////////////////////////////////
+// CFrameApp manages the application. It initializes the Win32++
+// framework when it is constructed, and create the main frame
+// window when it runs.
+class CFrameApp : public CWinApp
+{
+public:
+    CFrameApp();
+    virtual ~CFrameApp();
+
+protected:
+    // Virtual functions that override base class functions
+    virtual BOOL InitInstance();
+
+private:
+    CFrameApp(const CFrameApp&);                // Disable copy construction
+    CFrameApp& operator = (const CFrameApp&);   // Disable assignment operator
+
+    CMainFrame m_frame;
+};
+
+
+#endif // define FRAMEAPP_H
