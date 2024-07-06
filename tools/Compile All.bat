81d6cf722d0c5140e40c3d35d64f8d37dae92805
DavidNash2024 <161179606+DavidNash2024@users.noreply.github.com>
2024-07-06 17:05:42 +1000

    Version 9.6

--- tools/Compile All.bat
+++ tools/Compile All.bat
@@ -1,12 +1,12 @@
 REM  Run the complete set of Compile scripts
 
-cmd /c call "Compile CodeBlocks"
+cmd /c call "Compile CodeBlocks.bat"
 cmd /c call "Compile Embarcadero.bat"
-cmd /c call "Compile VS2005 Express"
-cmd /c call "Compile VS2008 Express"
-cmd /c call "Compile VS2010 Express"
-cmd /c call "Compile VS2013 Community"
-cmd /c call "Compile VS2015 Community"
-cmd /c call "Compile VS2019 Community"
-cmd /c call "Compile VS2022 Community"
+cmd /c call "Compile VS2005 Express.bat"
+cmd /c call "Compile VS2008 Express.bat"
+cmd /c call "Compile VS2010 Express.bat"
+cmd /c call "Compile VS2013 Community.bat"
+cmd /c call "Compile VS2015 Community.bat"
+cmd /c call "Compile VS2019 Community.bat"
+cmd /c call "Compile VS2022 Community.bat"
 
